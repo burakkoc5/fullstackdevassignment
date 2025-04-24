fb58cff5b1c3c255244e367fc09869f11a6c7ebc
Burak Koç <74610612+burakkoc5@users.noreply.github.com>
2025-04-24 23:20:42 +0300

    test: add generated Spring Boot test class

---
+++ src/test/java/com/repsy/fullstackdevassignment/FullStackDevAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repsy.fullstackdevassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackDevAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
